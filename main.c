b32ac223cae1662d3f78f14d9f00d1a58312ce63
ronp@winter <ron.peleg@trusteer.com>
2014-12-10 18:08:08 +0200

    first working malloc/free, with simple test in main

--- main.c
+++ main.c
@@ -59,15 +59,36 @@ int _strlen(const char *s)
 
 int main()
 {
+  void *p[4];
+  int i;
+
   _uart0_prints(hello);
   _uart0_prints(nl);
   _uart0_prints(message);
   ssize = _strlen(hello);
   _uart0_printi(ssize);
   _uart0_prints(nl);
-  _uart0_printx((int)(malloc(10)));
-  _uart0_prints(nl);
-  _uart0_printx((int)(malloc(10)));
-  _uart0_prints(nl);
+
+  /* simple malloc/free test: allocate 2 of 2 alignment sizes, release,
+   * allocate again. since free works as stack - second run we allocated a 32
+   * byte chunk for malloc 11/12, then we are stuck with 2 free chunks which are
+   * too small for 13/14, so we allocated from uncharted */
+  for(i=0; i<4; i++)
+  {
+    p[i] = malloc(11+i); /* first 2 will aling to 16, next 2 to 32 */
+    _uart0_printx((unsigned int)p[i]);
+    _uart0_prints(nl);
+  }
+
+  for(i=0; i<4; i++)
+    free(p[i]);
+
+  for(i=0; i<4; i++)
+  {
+    p[i] = malloc(11+i); /* first 2 will aling to 16, next 2 to 32 */
+    _uart0_printx((unsigned int)p[i]);
+    _uart0_prints(nl);
+  }
+
   _uart0_prints("bye.");
 }
--- mem/alloc.c
+++ mem/alloc.c
@@ -1,20 +1,58 @@
 #include "alloc.h"
 
+/* defined in linker script, actually located in heap-start and ram-end */
 extern unsigned int heap_start;
 extern unsigned int ram_end;
+static unsigned int align = 16; /* must be a power of 2 see malloc */
+
+typedef struct {
+  size_t size;
+  void* next; /* this one either points to the next free, or is the address returned to the caller */
+} chunk_t;
+
+static chunk_t free_list = {.size = 0L, .next = NULL};
 
 void *malloc(size_t nbytes)
 {
-  static void *next_chunk = &heap_start;
-  static void *heap_end = &ram_end;
+  static void *next_unmapped = &heap_start;
+  void *ret = NULL;
+  chunk_t* prev_iter = &free_list;
+  chunk_t* iter = free_list.next;
 
-  void *ret = next_chunk;
-  next_chunk += nbytes;
+  /* align nbytes, making sure we also add 4 bytes for chunk size info
+   * we assume 'align' is a power of 2 so the affect of adding (align-1) and
+   * then bitwise NAND (align-1) is to align up */
+  nbytes = (nbytes+sizeof(size_t)+(align-1)) & ~(align-1);
+
+  /* first look in free_list (currently first fit, TBI best fit via RB tree) */
+  while(iter && iter->size<nbytes)
+  {
+    prev_iter = iter;
+    iter = iter->next;
+  }
+
+  if(iter)
+  {
+    prev_iter->next = iter->next;
+    iter->next = 0L;
+    ret = &iter->next;
+  }
+  
+  /* if not found - take from next unmapped */
+  else if((next_unmapped+nbytes)<(void*)(&ram_end))
+  {
+    ((chunk_t*)next_unmapped)->size = nbytes;
+    ret = &((chunk_t*)next_unmapped)->next;
+    next_unmapped += nbytes;
+  }
   return ret;
 }
 
 void free(void *ptr)
 {
+  chunk_t *free_ptr = (chunk_t*)(ptr-sizeof(size_t));
+  free_ptr->next = free_list.next;
+  free_list.next = free_ptr;
 }
 
 void *calloc(size_t nmemb, size_t size)
